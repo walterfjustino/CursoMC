36a2780226007dd45db41f37d68e1fae1844861c
Walter Fernandes Justino <walterfjustino@gmail.com>
2020-04-21 21:44:25 -0300

    18-commit - Deletando uma categoria com DELETE

--- cursomc/src/main/java/com/cursomc/resources/CategoriaResources.java
+++ cursomc/src/main/java/com/cursomc/resources/CategoriaResources.java
@@ -40,4 +40,11 @@ public class CategoriaResources {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Categoria> delete(@PathVariable Integer id) throws ObjectNotFoundException {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
+
